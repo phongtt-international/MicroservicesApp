9ace8be68007c0099bc9a360f54015844af22920
phongtt <phongtt.international@gmail.com>
2020-12-20 12:16:58 +0700

    Add CatalogDatabaseSeting

--- src/Catalog/Catalog.API/Startup.cs
+++ src/Catalog/Catalog.API/Startup.cs
@@ -1,3 +1,5 @@
+using Catalog.API.Settings;
+using Catalog.API.Settings.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -5,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +27,11 @@ namespace Catalog.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Get CatalogDatabaseSetting from appsettings.js map to =>  CatalogDatabaseSettings class
+            services.Configure<CatalogDatabaseSettings>(Configuration.GetSection(nameof(CatalogDatabaseSettings)));
+            // sp => Service Provider, IOptions pattern
+            services.AddSingleton<ICatalogDatabaseSettings>(sp =>
+                sp.GetRequiredService<IOptions<CatalogDatabaseSettings>>().Value);
             services.AddControllers();
         }
 
