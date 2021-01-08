32f4cc3ae3fc0967808bb438515dfbf4ca0c141c
phongtt <phongtt.international@gmail.com>
2021-01-08 21:42:38 +0700

    Migrate data Order with entity framework core

--- src/Ordering/Ordering.API/Startup.cs
+++ src/Ordering/Ordering.API/Startup.cs
@@ -1,10 +1,14 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Ordering.Core.Repositories;
+using Ordering.Core.Repositories.Base;
+using Ordering.Infrastructure.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +29,18 @@ namespace Ordering.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            #region SqlServer Dependencies
+
+            //// use in-memory database
+            //services.AddDbContext<OrderContext>(c =>
+            //    c.UseInMemoryDatabase("OrderConnection"));
+
+            // use real database
+            services.AddDbContext<OrderContext>(c =>
+                c.UseSqlServer(Configuration.GetConnectionString("OrderConnection")), ServiceLifetime.Singleton); // we made singleton this in order to resolve in mediatR when consuming Rabbit
+
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
